501119be4841662e4f12b4ec3a1965559e4dbbfe
lubaek <60066060+lubaek@users.noreply.github.com>
2022-01-11 17:07:13 +0600

    styled confirmation component

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Confirmation() {
-	return <div className="container">Confirmation</div>;
+	return (
+		<div className="container">
+			<h2 className="confirmation">
+				You brought 0.019485 BTC <br /> for 500 AUD!
+			</h2>
+		</div>
+	);
 }
 
 export default Confirmation;
